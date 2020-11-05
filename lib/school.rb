f17ad9516221eaaa515eea0e17a03ec54528109c
Your Username <you@example.com>
2020-11-05 13:06:30 -0500

    commit tree 39cc6b3b474291371278fa4a15eb7eed253aaa16

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+require 'pry'
+
+class School 
+
+    attr_accessor :name, :roster
+
+
+def initialize(name)
+    @name = name 
+    @roster = {}
+
+    
+end  
+
+def add_student(student, grade) 
+  if  !@roster.key?(grade) 
+    @roster[grade] = []
+  end 
+    @roster[grade] << student
+end 
+
+def grade (grade)
+@roster[grade] 
+end 
+
+def sort
+  
+    #@roster.each { |key, value| key.sort} 
+    
+    #key.each {|value| a <=> b}
+    @roster.each do |key, value| 
+      @roster[key] = value.sort
+      
+    end 
+      
+  end 
+
+end 
